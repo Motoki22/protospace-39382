4b2ca3cc00afc8a849e8caf670867ba3658fbcc9
Motoki22 <mo.sato.nmnl@gmail.com>
2023-08-07 11:18:24 +0900

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,28 @@
+class CommentsController < ApplicationController
+before_action :set_prototype, only: [:create,:show]
+
+  def create
+    @comment = @prototype.comments.new(comment_params) 
+    if @comment.save
+      redirect_to @prototype
+    else
+      render "prototypes/show" 
+    end
+  end
+
+  def show
+    @comment = Comment.new
+  end
+
+  private
+
+  def set_prototype
+    @prototype = Prototype.find(params[:prototype_id])
+  end
+  
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
+
+  
+end
